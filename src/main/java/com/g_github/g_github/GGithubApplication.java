bf43b49a055d23caebbe123c52d72cc416584f0d
17350756762 <1259935303@qq.com>
2020-09-10 19:03:37 +0800

    第一次测试提交本地库

---
+++ src/main/java/com/g_github/g_github/GGithubApplication.java
@@ -0,0 +1,13 @@
+package com.g_github.g_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/g_github/g_github/GGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g_github.g_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
